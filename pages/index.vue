cfa0e257aa0777fdc57606657ffd95b69269543e
Billy Clark <billy_clark@sil.org>
2018-05-14 09:58:05 -0400

    needed to test ending slashes on relative links

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section>
     <h1>Welcome!</h1>
 
-    <a href="nodes">I'd like to see the nodes</a>
+    <a href="nodes/">I'd like to see the nodes</a>
     <a href="users">I'd like to see the users</a>
     <a href="tags">I'd like to see the tags</a>
   </section>
