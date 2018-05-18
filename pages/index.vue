99cd581f93db3fa647609c41d337fb230722408f
Billy Clark <billy_clark@sil.org>
2018-05-18 16:46:10 -0400

    needed ability to see named servers

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <a href="nodes">I'd like to see the nodes</a>
     <a href="users">I'd like to see the users</a>
     <a href="tags">I'd like to see the tags</a>
+    <a href="servers">I'd like to see the servers</a>
   </section>
 </template>
 
